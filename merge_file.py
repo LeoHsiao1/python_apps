8fc4beac990c164d25f8c118e723615609bf6b8d
LeoHsiao <leohsiao@foxmail.com>
2020-01-16 18:31:28 +0800

    将searchFIle()改为find_file()

--- merge_file.py
+++ merge_file.py
@@ -6,35 +6,30 @@
 import os
 import traceback
 
-from utils import Inputs, searchFile, test_run
+from utils import Inputs, find_file
 
 
-def main():
-    # 提示用户输入
-    print("该程序用于检索指定目录（包括子目录）下某种后缀名的文件，将它们合并成一个文件。")
-    Inputs.path = Inputs.input_path("请输入要检索的目录：")
-    Inputs.suffix = input("请输入要合并的这类文件的后缀名（例如'.txt'）：")
+# 提示用户输入
+print("该程序用于检索指定目录（包括子目录）下某种后缀名的文件，将它们合并成一个文件。")
+Inputs.path = Inputs.input_path("请输入要检索的目录：")
+Inputs.pattern = input("请输入要合并的这类文件的名字格式（比如'*.txt'）：")
 
-    # 设置保存检索结果的文件路径
-    result_file_name = "merge_result" + Inputs.suffix
+# 设置保存检索结果的文件路径
+result_file_name = "merge_result"
 
-    # 获得符合条件的文件列表
-    file_list = searchFile(Inputs.path, Inputs.suffix)
+# 获得符合条件的文件列表
+file_list = find_file(Inputs.path, pattern=Inputs.pattern)
 
-    # 读取每个文件的内容并合并
-    print("开始检索...")
-    with open(result_file_name, 'wb') as f:  # 以二进制模式打开，这样就不用考虑编码格式
-        for path in file_list:
-            try:
-                with open(path, 'rb') as raw_file:
-                    f.write(raw_file.read())
-                    print("已处理：{}".format(path))
-            except:
-                print("处理失败：{}".format(path))
-                traceback.print_exc()
+# 读取每个文件的内容并合并
+print("开始检索...")
+with open(result_file_name, 'wb') as f:  # 以二进制模式打开，这样就不用考虑编码格式
+    for path in file_list:
+        try:
+            with open(path, 'rb') as raw_file:
+                f.write(raw_file.read())
+                print("已处理：{}".format(path))
+        except:
+            print("处理失败：{}".format(path))
+            traceback.print_exc()
 
-    print("\n全部完成！\n数据保存为文件：{} ".format(result_file_name))
-
-
-if __name__ == "__main__":
-    test_run(main)
+print("\n全部完成！\n数据保存为文件：{} ".format(result_file_name))
--- resave_image.py
+++ resave_image.py
@@ -8,47 +8,43 @@ import traceback
 
 from PIL import Image
 
-from utils import Inputs, searchFile, test_run
+from utils import Inputs, find_file, test_run
 
 
-def main():
-    # 提示用户输入
-    print("该程序用于检索指定目录（包括子目录）下的所有.jpg图片，将它们另存到当前目录。")
-    Inputs.path = Inputs.input_path("请输入要检索的目录：")
-    Inputs.suffix = (".jpg",".jpeg",".png")
-
-    print("检索所有图片...")
-    file_list = searchFile(Inputs.path, Inputs.suffix)
-
-    # 创建一个保存被修改图片的文件夹
-    result_dir = os.path.join(os.getcwd(), "result")
-    os.makedirs(result_dir, exist_ok=True)
-
-    print("开始修改...")
-    for path in file_list:
-        try:
-            img = Image.open(path)
-
-            # 可能有些图片只是后缀名为jpg，实际上却不是RGB模式，这里把它们都转换成RGB模式
-            if img.mode != "RGB":
-                img = img.convert("RGB")
-
-            # 创建保存图片的子目录
-            result_path = path.replace(Inputs.path, result_dir, 1)
-            result_path = result_path[:result_path.rfind('.')] + ".jpg"
-            os.makedirs(os.path.dirname(result_path), exist_ok=True)
-
-            # 保存图片
-            img.save(result_path, quality=95)
-            print("已保存：{}".format(result_path))
-            """将其它后缀名的图片转换成jpg图片时，可能会覆盖同名的jpg图片"""
-
-        except:
-            print("处理失败：{}".format(path))
-            traceback.print_exc()
-
-    print("\n全部完成。\n")
+# 提示用户输入
+print("该程序用于检索指定目录（包括子目录）下的所有.jpg图片，将它们另存到当前目录。")
+Inputs.path = Inputs.input_path("请输入要检索的目录：")
+Inputs.pattern = ("*.jpg", "*.JPG", "*.jpeg", "*.png")
 
 
-if __name__ == "__main__":
-    test_run(main)
+print("检索所有图片...")
+file_list = find_file(Inputs.path, "*.jpg")
+
+# 创建一个保存被修改图片的文件夹
+result_dir = os.path.join(os.getcwd(), "result")
+os.makedirs(result_dir, exist_ok=True)
+
+print("开始修改...")
+for path in file_list:
+    try:
+        img = Image.open(path)
+
+        # 可能有些图片只是后缀名为jpg，实际上却不是RGB模式，这里把它们都转换成RGB模式
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+
+        # 创建保存图片的子目录
+        result_path = path.replace(Inputs.path, result_dir, 1)
+        result_path = result_path[:result_path.rfind('.')] + ".jpg"
+        os.makedirs(os.path.dirname(result_path), exist_ok=True)
+
+        # 保存图片
+        img.save(result_path, quality=95)
+        print("已保存：{}".format(result_path))
+        """将其它后缀名的图片转换成jpg图片时，可能会覆盖同名的jpg图片"""
+
+    except:
+        print("处理失败：{}".format(path))
+        traceback.print_exc()
+
+print("\n全部完成。\n")
