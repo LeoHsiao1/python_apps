6dd50edca87186613b797377232a4dcbf44660b7
LeoHsiao <leohsiao@foxmail.com>
2020-01-19 13:34:29 +0800

    增加replace.py，用于正则替换

---
+++ scripts/replace.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+替换文件中的字符串，基于正则语法。
+
+Sample：
+python3 replace.py --file 1.py --src '([\u4e00-\u9fa5])(\w)' --dst '$1 $2'
+'''
+from utils import replace
+import argparse
+
+
+parser = argparse.ArgumentParser(description=r"""This script is use to replace string in a file. Sample: python3 replace.py --file 1.py --src '([\u4e00-\u9fa5])(\w)' --dst '$1 $2' """)
+parser.add_argument('--file', help='a valid file path', type=str, required=True)
+parser.add_argument('--src', help='the source string, which is a regular expression.', type=str, required=True)
+parser.add_argument('--dst', help='the destination string', type=str, required=True)
+parser.add_argument('--encoding', help='the encoding of the original file, which is utf-8 by default.', type=str, default='utf-8')
+args = parser.parse_args()
+
+
+try:
+    # read the file
+    with open(args.file, 'r', encoding=args.encoding) as f:
+        text = f.read()
+        print('Handling file: {} ...'.format(args.file), end='\t')
+
+    # handling
+    result = replace(text, args.src, args.dst)
+
+    # save the result
+    with open(args.file, 'w', encoding=args.encoding) as f:
+        f.write(result)
+        print('done')
+
+except Exception as e:
+    print('Error: {}'.format(str(e)))
--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -2,3 +2,4 @@
 
 from .github.LeoHsiao1.Python_utils.utils._io import Inputs
 from .github.LeoHsiao1.Python_utils.utils._os import find_file
+from .github.LeoHsiao1.Python_utils.utils._re import replace
