6672ba81e1dfb27d70af92a088f8d60825a4b116
LeoHsiao <leohsiao@foxmail.com>
2020-02-18 20:20:27 +0800

    取消用PIL放大图片

--- scripts/adjust_images.py
+++ scripts/adjust_images.py
@@ -13,8 +13,7 @@ from utils import Inputs, find_file
 
 print("""该脚本的用途：检查目标目录（包括子目录）下的所有图片，如果它们不符合以下特征则自动修改：
 - 后缀名为jpg
-- 图片高度至少为1080
-- 图片的元数据中，只保存标签，其它可以丢弃""")
+- 只保留图片元数据中的标签""")
 
 Inputs.path = Inputs.input_path('请输入目标目录：')
 
@@ -40,12 +39,12 @@ for path in file_list:
         if img.mode != 'RGB':
             img = img.convert('RGB')
         
-        # 如果原图片的高度小于1080，则放大至1080
-        if img.size[1] < 1080:
-            width, height = img.size
-            times = 1080 / height
-            new_size = round(width * times), round(height * times)
-            img = img.resize(new_size)
+        # # 如果原图片的高度小于1080，则放大至1080（这样放大会产生锯齿）
+        # if img.size[1] < 1080:
+        #     width, height = img.size
+        #     times = 1080 / height
+        #     new_size = round(width * times), round(height * times)
+        #     img = img.resize(new_size)
 
         # 生成新图片的保存路径
         new_path = path[:path.rfind('.')] + new_suffix
