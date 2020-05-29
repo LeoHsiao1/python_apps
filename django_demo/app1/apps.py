8923a5715cd5d0ffcd45debc57262a35e02ab69f
Will <1346508197@qq.com>
2020-05-29 15:43:07 +0800

    撤销AppConfig改名，与默认格式一致

--- django_demo/app1/apps.py
+++ django_demo/app1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TheAppConfig(AppConfig):
+class App1Config(AppConfig):
     name = 'app1'              # 该 app 的路径名
     verbose_name = '示例应用'  # 该 app 的别名
