d9ed6cd49352aba82ea24a0c060f4cb4d81774e3
Will <1346508197@qq.com>
2020-05-29 15:21:04 +0800

    改名为 TheAppConfig

--- django_demo/app1/apps.py
+++ django_demo/app1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class app1Config(AppConfig):
+class TheAppConfig(AppConfig):
     name = 'app1'              # 该 app 的路径名
     verbose_name = '示例应用'  # 该 app 的别名
--- django_demo/django_demo/settings.py
+++ django_demo/django_demo/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1.apps.app1Config',     # 启用一个 Web app
+    'app1.apps.TheAppConfig',     # 启用一个 Web app
 ]
 
 MIDDLEWARE = [
