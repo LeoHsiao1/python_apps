4d018419b7302a757ac3d8f30facd29eb2522d53
Will <1346508197@qq.com>
2020-12-26 17:40:18 +0800

    优化 DATABASES 的配置

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -54,24 +54,25 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # 使用 SQLite 数据库
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    # 'default': {
-    #     # 使用 MySQL 数据库
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'db1',
-    #     'USER': 'root',
-    #     'PASSWORD': '******',
-    #     'HOST': '10.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-    #         'charset': 'utf8mb4'
-    #     }
-    # }
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db1',
+        'USER': 'root',
+        'PASSWORD': '******',
+        'HOST': '10.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'charset': 'utf8mb4'
+        }
+    }
 }
+
+DATABASES = {'default': DATABASES['sqlite']}
 
 TEMPLATES = [
     {
